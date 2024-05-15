1563c7f0fa07f1e44ff7dd04f3be42bfdbd92db6
Isbat-Bin-Hossain <isbat31416@gmail.com>
2024-05-16 01:52:00 +0600

    Add prisma to the project

---
+++ prisma/migrations/20240515194454_initial_migration/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Link` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `originalUrl` VARCHAR(191) NOT NULL,
+    `shortUrl` VARCHAR(191) NOT NULL,
+    `userId` INTEGER NOT NULL,
+
+    UNIQUE INDEX `Link_shortUrl_key`(`shortUrl`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Link` ADD CONSTRAINT `Link_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
