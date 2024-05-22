22a11f0d50ea52bc6d64c78fa6d3f197dbc4fdfc
Isbat-Bin-Hossain <isbat31416@gmail.com>
2024-05-22 14:42:21 +0600

    Add getUser controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -38,7 +38,7 @@ export const registerUser = async (req: Request, res: Response) => {
     })
 
     res.status(201).json({ user })
-  } catch (error) {
+  } catch (error: unknown) {
     console.error(error)
     res.status(500).json({ message: 'Internal Server Error' })
   }
@@ -59,8 +59,19 @@ export const logoutUser = (req: Request, res: Response) => {
 }
 
 //Get User
-export const getUser = (req: Request, res: Response) => {
-  res.json({
-    data: 'Get User ',
-  })
+export const getUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params
+    const user = await prisma.user.findUnique({
+      where: { id: parseInt(id) },
+    })
+    if (user) {
+      res.status(200).json({ user })
+    } else {
+      res.status(404).json({ message: 'User not found' })
+    }
+  } catch (error: unknown) {
+    console.error(error)
+    res.status(500).json({ message: 'Internal Server Error' })
+  }
 }
